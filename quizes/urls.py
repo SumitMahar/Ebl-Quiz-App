5ee7c511045073c36bb9b93f538e20226ab516b0
Sumit Mahar <SumitMahar78@gmail.com>
2022-01-04 22:10:28 +0530

    added a url path

--- quizes/urls.py
+++ quizes/urls.py
@@ -11,9 +11,8 @@ from .views import (
 app_name = 'quizes'
 
 urlpatterns = [
-    path('abt/', about_view, name='about_view'),
-    path('quiz/about/', about_view, name='about'),
-    path('', QuizListView.as_view(), name='quiz_home' ),
+    path('', about_view, name='quiz_home' ),
+    path('about/', about_view, name='about'),
     path('quiz/<pk>/', quiz_view, name='quiz_view' ),
     path('quiz/<pk>/data/', quiz_data_view, name='quiz_data_view'),
     path('quiz/<pk>/save', save_quiz_view, name='quiz_save_view' ),
